aae44432415efbe643ec70f78f4e237b8c4690cb
achaljhawar <achaljhawar03@gmail.com>
2024-01-27 23:03:29 +0530

    auth

--- src/server/main.js
+++ src/server/main.js
@@ -1,9 +1,34 @@
 import express from "express";
+import session from "express-session";
 import ViteExpress from "vite-express";
-
+function isLoggedIn(req,res,next) {
+  req.user ? next() : res.redirect("/login")
+}
+import dotenv from 'dotenv';
+dotenv.config();
+import './auth.js';
+import passport from "passport";
 const app = express();
 
-app.get("/hello", (req, res) => {
+app.use(session({ secret: "bruh"}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/login/auth/google', 
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+)
+app.get('/login/google/callback', 
+  passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/login/auth/failure',
+    scope: ['profile', 'email']
+  })
+)
+app.get('/login/auth/failure', (req,res) => {
+  res.send('Failed to authenticate..')
+})
+app.get("/",isLoggedIn, (req, res) => {
   res.send("Hello Vite + React!");
 });
 
