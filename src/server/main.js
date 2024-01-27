72b6014ed188649e4f33f8c9d651e31da54ee48e
achaljhawar <achaljhawar03@gmail.com>
2024-01-28 04:14:30 +0530

    feat: auth and crud

--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,37 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import { supabase } from "./database/db.js";
+import session from "express-session";
+import dotenv from 'dotenv';
+dotenv.config();
+import './auth.js';
+import passport from "passport";
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
+app.use(session({ secret: "bruh"}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.get('/login/auth/google', 
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+)
+app.get('/login/google/callback', 
+  passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/login/auth/failure',
+    scope: ['profile', 'email']
+  })
+)
+app.get('/login/auth/failure', (req,res) => {
+  res.send('Failed to authenticate..')
+})
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+app.get('/api/auth/check', (req, res) => {
+  res.json({ isAuthenticated: req.isAuthenticated(), user: req.user });
+});
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
 );
